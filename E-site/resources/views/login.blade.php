68aa2554572188cd2a00f1a869ac2941caa1d72d
sheikhjaber0424 <55077645+sheikhjaber0424@users.noreply.github.com>
2022-07-09 09:56:51 -0800

    bootstrap added

---
+++ E-site/resources/views/login.blade.php
@@ -0,0 +1,5 @@
+{{-- used extends so, it brings the properties of parent class into the child class(login) --}}
+@extends('master') 
+@section('content')
+<h1>Login Page</h1>
+<button type="button" class="btn btn-primary">Primary</button>
