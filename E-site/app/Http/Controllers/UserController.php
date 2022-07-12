dadeb8d9e095919f6d8149dbc82ab524d2d26473
sheikhjaber0424 <55077645+sheikhjaber0424@users.noreply.github.com>
2022-07-12 15:00:39 -0800

    login done

--- E-site/app/Http/Controllers/UserController.php
+++ E-site/app/Http/Controllers/UserController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     function login(Request $request)
     {
-        return $request->input();
-    }
+        $user =  User::where(['email'=>$request->email])->first();
+       
+        if(!$user || !Hash::check($request->password, $user->password))
+        {
+            return "Username or password is not matched";
+        }
+
+        else
+        {
+            $request->session()->put('user',$user);
+            return redirect('/');
+        }
+        
+    } 
 }
+ 
--- E-site/routes/web.php
+++ E-site/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ Route::get('/login', function () {
 });
 
 Route::post('/login',[UserController::class,'login']);
+
+Route::get('/',[ProductController::class,'index']);
